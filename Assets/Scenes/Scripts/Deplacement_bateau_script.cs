be38baaace95e860dbfc09993cece4fcf46f5c96
Matt3466 <a199577@gmail.com>
2020-11-30 19:12:42 +0100

    Deplacement_Bateau_N1
    premiere version de deplacement du bateau et de son comportement face au vent

---
+++ Assets/Scenes/Scripts/Deplacement_bateau_script.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Deplacement_bateau_script : MonoBehaviour /* script qui doit gérer toute les forces qui s'exercent sur le bateau
+                                                   pour l'instant il doit être capable de réceptionner la force du vent
+                                                   ainsi que sa direction mais il doit aussi être capable potentiellement
+                                                   de prendre en compte la force et la direction des vagues et peut-être
+                                                   des courants marins*/
+
+                                                    // mode d'emplois:
+                                                    // il faut attacher ce script au GameObject Bateau 
+{
+    public float Direction_Du_Bateau;
+    public float Vitesse_Du_Bateau;
+    private float Force_Du_Vent;
+    private float Direction_Du_Vent;
+    private Vector3 Orientation_Bateau;
+    private Vector3 Deplacement_Bateau;
+
+    GameObject Vent;
+    // Start is called before the first frame update
+    void Start()
+    {
+       Vent = GameObject.Find("Vent");              //il faut dans la scène un GameObject "Vent" qui aura une direction et une force
+        Orientation_Bateau = transform.eulerAngles.normalized; // permet de donner à Orientiatio_Bateau les valeurs de rotation du bateau
+        Deplacement_Bateau = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Force_Du_Vent = Vent.GetComponent<Vent_Script>().Force_Du_Vent;             //Récupère la force du Vent
+        Direction_Du_Vent = Vent.GetComponent<Vent_Script>().Direction_Du_Vent;     // récupère l'orientation en z du Vent
+        transform.eulerAngles = Orientation_Bateau;                                 // permet de modifier l'axe z du Bateau en fonction de l'orientation du vent
+        Orientation_Bateau.z = Direction_Du_Vent;                                   // idem
+
+
+        transform.position = Deplacement_Bateau;
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Deplacement_Bateau.y += Vitesse_Du_Bateau;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Deplacement_Bateau.y -= Vitesse_Du_Bateau;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Deplacement_Bateau.x -= Vitesse_Du_Bateau;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Deplacement_Bateau.x += Vitesse_Du_Bateau;
+        }
+    }
+}
---
+++ Assets/Scenes/Scripts/Vent_Script.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vent_Script : MonoBehaviour /* Script qui gère la force et la direction du vent*/
+
+                                         // mode d'emplois:
+                                         // il faut attacher ce script à un GameObject "Vent" ce GameObject peu être un Empty et doit avoir le nom "Vent"
+{
+    public float Force_Du_Vent = 10f;
+    public float Direction_Du_Vent;
+    private Vector3 Orientation_Du_Vent;
+
+
+    GameObject Bateau;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("Bateau");
+        Orientation_Du_Vent = transform.eulerAngles.normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Direction_Du_Vent = transform.rotation.eulerAngles.z;
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            Orientation_Du_Vent.z += 5;
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            Orientation_Du_Vent.z -= 5;
+        }
+        transform.eulerAngles = Orientation_Du_Vent;
+    }
+}
