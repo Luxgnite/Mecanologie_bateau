a77154651934656b257dfd8f378ca8a3fefe027d
Luxgnite <ewan.dubois@live.fr>
2020-12-07 21:50:12 +0100

    Transitions fade in / out changement scènes
    * Mise en place d'une image d'UI dont l'opacité diminue au chargement d'une scène et augmente lorsque demandé pour charger une autre scène, permettant de faire un effet de "fade in" et "fade out"
    * Le GameManager se charge de localiser l'animator de l'image UI chargée du fade [! PAS SECURISÉ POUR LE NULLPOINTEREXCEPTION]
    
    /!\ Changement de la logique pour le chargement de la vue "Deplacement_joueur", à retoucher

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,9 @@ public class GameManager : MonoBehaviour
     public delegate void LieuAction(Lieu nouveauLieu, Lieu ancienLieu);
     public static event LieuAction OnChangementLieu;
 
+    //Animator de l'objet utilisé pour faire des fade in/out 
+    public Animator fadeAnimator;
+
     //Lieu dans lequel le joueur se situe
     public Lieu lieuJoueur;
     /// <summary>
@@ -33,7 +36,7 @@ public class GameManager : MonoBehaviour
                 lieuJoueur = value;
                 //Envoi de l'événement pour signaler un changement de lieu
                 OnChangementLieu(lieuJoueur, temp);
-                StartCoroutine(AfficherAction(lieuJoueur.nomLieu));
+                afficherAction(lieuJoueur.nomLieu);
             }    
         }
     }
@@ -48,26 +51,45 @@ public class GameManager : MonoBehaviour
 
         DontDestroyOnLoad(this);
 
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    //Coroutine pour effectuer afficherAction avec du délai
-    IEnumerator AfficherAction(string nomAction)
+    //Lorsqu'une scène est chargée
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        yield return new WaitForSeconds(1f);
-        afficherAction(nomAction);
+        fadeAnimator = GameObject.Find("Fade").GetComponent<Animator>();
     }
 
     //Afficher l'action à effectué sur le lieu sélectionné
     void afficherAction(string nomTache)
     {
-        SceneManager.LoadScene("Scenes/ScenesTest/Taches/" + nomTache);
+        chargerScene("Scenes/ScenesTest/Taches/" + nomTache);
     }
 
     /// <summary>
     /// Afficher la scène de la carte du bateau
     /// </summary>
-    public static void afficherCarteBateau()
+    public void afficherCarteBateau()
     {
-        SceneManager.LoadScene("Scenes/ScenesTest/Deplacement_personnage");
+        chargerScene("Scenes/ScenesTest/Deplacement_personnage");
+    }
+    
+    /// <summary>
+    /// Charge la scène correspondant au chemin
+    /// </summary>
+    /// <param name="chemin">Chemin vers la scène à charger. Ex : "Scenes/Scene1" </param>
+    /// <param name="delaiLatence">Temps en seconde de la latence. Si non spécifié, il est égal à 1f </param>
+    public void chargerScene(string chemin, float delaiLatence = 1f)
+    {
+        StartCoroutine(ChargerSceneDelai(chemin, delaiLatence));
+    }
+
+    //Coroutine pour effectuer charger une scène avec du délai
+    IEnumerator ChargerSceneDelai(string chemin, float delaiLatence)
+    {
+        yield return new WaitForSeconds(delaiLatence-1);
+        fadeAnimator.SetTrigger("Start");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(chemin);
     }
 }
